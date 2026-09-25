c6b359b6fa6a521ac968bdb893549f6415431c55
ArafatAli-07 <aliarafat801111@gmail.com>
2026-09-26 01:38:56 +0530

    fix signup page ui and write frontend logic

--- frontend/src/app/signup/page.jsx
+++ frontend/src/app/signup/page.jsx
@@ -2,48 +2,90 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import axios from 'axios';
 import { UserPlus } from 'lucide-react';
 import Image from 'next/image';
-import React from 'react'
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react'
+import { toast } from 'sonner';
 
 const Signup = () => {
+
+  const [input, setinput] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const changeEventHandler = (e) => {
+    setinput({ ...input, [e.target.name]: e.target.value });
+  }
+
+  const router = useRouter();
+
+  const signupHandler = async (e) => {
+    e.preventDefault();
+    // console.log(input);
+    try {
+      const res = await axios.post('http://localhost:8000/api/v1/user/register', input , {
+        headers:{
+          'Content-Type':'application/json'
+        },
+        withCredentials:true
+      });
+      if(res.data.success){
+        toast.success(res.data.message);
+        router.replace("/login")
+      }
+    } catch (error) {
+      // console.log(error);
+      toast.error(error.response.data.message);
+    }
+  }
+
   return (
-    <div className='flex items-center w-screen h-screen justify-center'>
-      <form action="" className='shadow-lg flex flex-col p-8 rounded-2xl bg-black text-white min-h-[75vh] min-w-[35vw]'>
+    <div className='flex items-center w-screen h-screen justify-center bg-black'>
+      <form onSubmit={signupHandler} className='shadow-sm shadow-blue-400 flex flex-col p-8 rounded-2xl bg-black text-white min-h-[80vh] min-w-[35vw] max-w-[90vw] md:min-w-[40vw]'>
         <div className='my-3 w-full'>
-          <Image className='w-50 h-35 mx-auto' src="/logo.png" alt="Logo"  width={100} height={100}/>
-          <h1 className='text-center font-bold font-serif'>SignUp</h1>
-          <p className='text-center text-gray-300'>Connect with friends and create memories.</p>
+          <Image className='w-50 h-35 mx-auto' src="/logo.png" alt="Logo" width={100} height={100} />
+          <h1 className='text-center text-xl font-bold font-serif'>SignUp</h1>
+          <p className='text-center text-gray-300 my-2'>Connect with friends and create memories.</p>
         </div>
         <div className='flex flex-col gap-3'>
           <Label>Username</Label>
           <Input
-          type='text'
-          name='username'
-          placeholder='Username'
-          className='' 
+            type='text'
+            name='username'
+            value={input.username}
+            onChange={changeEventHandler}
+            placeholder='Username'
+            required
           />
 
           <Label className="mt-3">Email</Label>
           <Input
-          type='text'
-          name='email'
-          placeholder='Email'
-          className='' 
+            type='email'
+            name='email'
+            value={input.email}
+            onChange={changeEventHandler}
+            placeholder='Email'
+            required
           />
 
           <Label className="mt-3">Psssword</Label>
           <Input
-          type='text'
-          name='password'
-          placeholder='Password'
-          className='' 
+            type='password'
+            name='password'
+            value={input.password}
+            onChange={changeEventHandler}
+            placeholder='Password'
+            required
           />
-          <Button className='bg-blue-400  border-blue-200 hover:border-2 p-4 font-bold font-serif text-md mt-3'><UserPlus size={18} />Sign Up</Button>
-          
+          <Button type='submit' className='bg-blue-400  border-blue-200 hover:border-2 p-4 font-bold font-serif text-md mt-3'><UserPlus size={18} />Sign Up</Button>
+
           <span className="text-right text-xs text-gray-300 font-serif">
-               {"Already have an account?"} <a href="/login" className="underline text-blue-400 mt-4 mr-3 font-bold font-serif">Login</a>
-               </span>
+            {"Already have an account?"} <a href="/login" className="underline text-blue-400 mt-4 mr-3 font-bold font-serif">Login</a>
+          </span>
         </div>
       </form>
     </div>
